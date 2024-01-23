048bd458b5aeac56bb768c4a47a97fffd160c629
Antwan <94127380+Antwannnn@users.noreply.github.com>
2024-01-23 14:38:08 +0100

    finished set-up

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Home = () => {
   return (
     <section className="h-full flex flex-col justify-between items-center">
-        <h1 className="text-4xl text-secondary_light">Welcome to <span className="text-primary_light">blader</span>.</h1>
-
+        <h1 className="text-4xl text-secondary_light">Welcome to <span className="text-primary_light font-bold">blader</span>.</h1>
+        <p className="text-secondary_light text-xl">The ultimate typing speed tester.</p>
     </section>
   )
 }
