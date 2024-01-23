33be77ff99f4123a31073e20d93fa369db6c3f8a
Antwan <94127380+Antwannnn@users.noreply.github.com>
2024-01-23 08:34:02 +0100

    removed default style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  
   title: "blader | Home",
   description: "Discover blader, the ultimate typing test and racer game.",
 };
